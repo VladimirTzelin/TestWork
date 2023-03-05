e4df25b1619e9f9fa88a954e24dd45ee48e07e2f
Vladimir Tselin <vtzelin@mail.ru>
2023-03-05 18:15:57 +0400

    Добавлен метод формирования строковых значений через консоль и сохранение в текстовый файл

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿void CreateArraytxt()
+{
+    StreamWriter f = new StreamWriter("text.txt");
+    string str = "0";
+    while (str != string.Empty) // Заполняем массив и сохраняем в файл txt.
+    {
+        Console.Write("Введите значение> ");
+        str = Console.ReadLine()!;
+        f.WriteLine(str);
+    }
+    f.Close();
+}
